d676a576d5a381f7749d214846b89f78fb4a03d1
Abhay Nagesh <abhay.nagesh@synapse.com>
2018-04-09 15:59:15 +0530

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -71,6 +71,25 @@ userSchema.statics.findByToken = function (token) {
   });
 };
 
+userSchema.statics.findByCredentials = function(email, password) {
+  let User = this;
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res){
+          resolve(user);
+        }else {
+          reject(err);
+        }
+      })
+    });
+  })
+}
+
 userSchema.pre('save',function(next) {
   let user = this;
   // console.log(user);
