cc02d766820d4b0ceaeb1cbc96ce92760b37b209
Abhay Nagesh <abhay.nagesh@synapse.com>
2018-04-09 12:10:25 +0530

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-}
+let pass = '123abc';
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(pass,salt,(err,hash) => {
+    console.log(hash);
+  })
+});
 
-let token = jwt.sign(data,'123abc');
-console.log(token);
-let decode = jwt.verify(token,'123abc');
-console.log('decode:',decode);
+let hashPass = '$2a$10$2oxS2UYq8Si0aPuHVAP7LeWTftLJfh6SGpWfZnvb3WhQ4RkMbCG0y';
+
+bcrypt.compare(pass, hashPass, (err,res) => {
+  console.log(res);
+});
+// let data = {
+//   id: 10
+// }
+//
+// let token = jwt.sign(data,'123abc');
+// console.log(token);
+// let decode = jwt.verify(token,'123abc');
+// console.log('decode:',decode);
 // let msg = 'I am number 4';
 // let hash = SHA256(msg).toString();
 // console.log(`Message: ${msg}\nHash: ${hash}`);
